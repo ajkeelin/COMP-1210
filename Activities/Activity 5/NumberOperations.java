3fc79fda1d3a57cf4bd31d1f726e2c7d6073bce8
ajkeelin <ajk0033@tigermail.auburn.edu>
2017-12-18 20:28:54 -0600

    Add files via upload

---
+++ Activities/Activity 5/NumberOperations.java
@@ -0,0 +1,47 @@
+/**
+ *
+ */
+public class NumberOperations{
+   private int number, compareNumber;
+   public NumberOperations(int numberIn) {
+      number = numberIn;
+   }
+   public int getValue() {
+      return number; 
+   }
+   public String oddsUnder() {
+      String output = "";
+      int i = 0;
+      while (i < number) {
+         if(i % 2 != 0) {
+            output += i + "\t";
+         }
+         i++;
+      }
+      return output;   
+   }
+   public String powersTwoUnder() {
+      String output = "";
+      int powers = 1;
+      while (powers < number) {
+         output += powers + "\t"; //concatenate to output
+         powers = powers * 2; // get next power of 2
+      }
+      return output;   
+   }
+   public int isGreater(int compareNumberIn) { 
+      compareNumber = compareNumberIn;
+      if (number > compareNumber) {
+         return 1;
+      }   
+      else if (number < compareNumber) {
+         return -1;
+      }   
+      else {
+         return 0;
+      }         
+   }
+   public String toString() {
+      return number + "";
+   }       
+   }       
